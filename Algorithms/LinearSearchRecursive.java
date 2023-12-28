d3561cb58119c039070be618ec50c1790cf7fe29
Shambhavi <shambhavirani625@gmail.com>
2023-12-28 19:51:49 +0530

    LinearSearchRecursive in java

---
+++ Algorithms/LinearSearchRecursive.java
@@ -0,0 +1,17 @@
+class LinearSearchRecursive {
+       public static int linearSearch(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        }
+        return linearSearch(arr, target, index + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println(linearSearch(arr, 4, 0));
+    }
+    
+}
