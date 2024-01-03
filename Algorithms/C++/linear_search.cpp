a401de9a2ebb9ce5cb4feb6aad068cb025e1175d
raiyan-wasim <gokuendou12@gmail.com>
2024-01-03 09:34:28 +0530

    linear search in cpp

---
+++ Algorithms/C++/linear_search.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int search(int *arr, int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << search(arr, 8, 5);
+}
