5eeb579fe107a174a175fa6b5e1b2051d95955ff
vikram0088 <kv97924413@gmail.com>
2024-01-09 21:10:08 +0530

    Add reverse_string in cpp

---
+++ Algorithms/C++/reverse_string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse_string(char *string, int size)
+{
+    char temp;
+
+    for (int i = 0; i < size / 2; i++)
+    {
+        temp = string[i];
+        string[i] = string[size - i - 1];
+        string[size - i - 1] = temp;
+    }
+}
+
+int main()
+{
+    char string[] = "Hello World!";
+    int size = sizeof(string) / sizeof(string[0]) - 1; // -1 to exclude NULL
+
+    reverse_string(string, size);
+    cout << string;
+
+    return 0;
+}
