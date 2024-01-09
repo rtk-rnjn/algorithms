a0a85f9cec2972d7f22744395079ede20b00be6e
vikram0088 <kv97924413@gmail.com>
2024-01-09 21:04:24 +0530

    sum_of_digits in cpp

---
+++ Algorithms/C++/sum_of_digits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_of_digits(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
+
+int main () {
+    int number = 12345;
+    
+    int result = sum_of_digits(number);
+    cout << "Sum of digit: " << result << endl;
+
+    return 0;
+}
