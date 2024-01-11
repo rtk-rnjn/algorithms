525b9256361113e9d368b0756dd791147dc27b40
abhirai7 <abhrai5031k@gmail.com>
2024-01-11 16:27:47 +0530

    387. First Unique Character in a String

---
+++ CP/LeetCode/CPP/387.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int size = s.size();
+        int max = int(s[0]);
+        for(int i = 0;i<size;i++){
+            if(int(s[i])>max){
+                max = s[i];
+            }
+        }
+        int hash[max+1];
+        for(int i = 0;i<max+1;i++){
+            hash[i] = 0;
+        }
+        for(int i = 0;i<size;i++){
+            hash[int(s[i])] +=1;
+        }
+        for(int i = 0;i<size;i++){
+            if( hash[int(s[i])] ==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    string s = "leetcode";
+
+    Solution testcase;
+    int result = testcase.firstUniqChar(s);
+    cout<<result<<endl;
+    return 0;
+}
