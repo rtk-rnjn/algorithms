c435c70bccb52c736c469a467141824901a7bb99
Ritik Ranjan <ritik0ranjan@gmail.com>
2024-01-14 14:53:22 +0530

    Lint CPP

--- CP/LeetCode/CPP/13.cpp
+++ CP/LeetCode/CPP/13.cpp
@@ -1,38 +1,43 @@
 // Roman to Integer
-#include<iostream>
-#include<unordered_map>
-#include<string>
+#include <iostream>
+#include <unordered_map>
+#include <string>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int romanToInt(string s) {
+    int romanToInt(string s)
+    {
         int result = 0;
-        unordered_map<char,int> symbol{
+        unordered_map<char, int> symbol{
             {'I', 1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000}
-        };
-        for(int i = 0;i<s.size()-1;i++){
-            if(symbol[s[i]]<symbol[s[i+1]]){
-                result-=symbol[s[i]];
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}};
+        for (int i = 0; i < s.size() - 1; i++)
+        {
+            if (symbol[s[i]] < symbol[s[i + 1]])
+            {
+                result -= symbol[s[i]];
             }
-            else{
-                result+=symbol[s[i]];
+            else
+            {
+                result += symbol[s[i]];
             }
         }
-        result += symbol[s[s.size()-1]];
+        result += symbol[s[s.size() - 1]];
         return result;
     }
 };
 
-int main(){
+int main()
+{
     Solution s;
-    cout<<s.romanToInt("MCMXCIV");
+    cout << s.romanToInt("MCMXCIV");
     return 0;
 }
