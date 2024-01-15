ad2e675cd765b0a3d92bc25e17e0eecc71ce0d95
Ritik Ranjan <ritik0ranjan@gmail.com>
2024-01-15 15:58:02 +0530

    Update StringReverse.java

--- Algorithms/Java/StringReverse.java
+++ Algorithms/Java/StringReverse.java
@@ -1,25 +1,24 @@
- //String Reverse using String Builder...
+// String Reverse using String Builder
+
 import java.util.*;
+
 public class StringReverse {
-   public static void main(String args[])
-   {
-    Scanner sc = new Scanner(System.in);
-    StringBuilder sb = new StringBuilder(sc.nextLine());   //----> Using StringBuilder class..
-    
-    for(int i=0; i<sb.length()/2; i++)
-    {
-        int front = i;
-        int back = sb.length() - i - 1;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(sc.nextLine());
 
-        char frontChar = sb.charAt(front);
-        char backChar = sb.charAt(back);
+        for (int i = 0; i < sb.length() / 2; i++) {
+            int front = i;
+            int back = sb.length() - i - 1;
 
-        sb.setCharAt(front, backChar);
-        sb.setCharAt(back, frontChar);
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+
+        sc.close();
+        System.out.println(sb);
     }
-
-    sc.close();
-    System.out.println(sb);
-
-   }
 }
