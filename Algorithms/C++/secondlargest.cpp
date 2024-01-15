34d2dc3f5c0b93c4deb0836ff271a166009c687e
vikram0088 <155892330+vikram0088@users.noreply.github.com>
2024-01-15 18:46:57 +0530

    Update secondlargest.cpp

--- Algorithms/C++/secondlargest.cpp
+++ Algorithms/C++/secondlargest.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+
 #include <climits>
 
 using namespace std;
+int secondlargest(int arr[], int size)
+{
 
-int main(){
-    
-    int arr[]= {5,4,6,8,9,7,9,5,3,6};
+    int slargest = INT_MIN;
+    int largest = arr[0];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > largest)
+        {
+            slargest = largest;
+            largest = arr[i];
+        }
+
+        if (largest != arr[i] && slargest < arr[i])
+        {
+            slargest = arr[i];
+        }
+    }
+    return slargest;
+}
+int main()
+{
+
+    int arr[] = {4, 5, 6, 7, 45, 8, 9, 8};
+
+    int size = 8;
+
+    int ans = secondlargest(arr, size);
+
+    cout << "the second largest element in array is :" << ans << endl;
 }
