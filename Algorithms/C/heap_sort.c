a903d3eeadec98631a7b8325c6f9f5ebc12879d8
rtk-rnjn <ritik0ranjan@gmail.com>
2024-01-20 21:40:56 +0530

    + [c] heap_sort

---
+++ Algorithms/C/heap_sort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void heapify(int arr[], int size, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+
+    if (right < size && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, size, largest);
+    }
+}
+
+void heap_sort(int arr[], int size)
+{
+    int i;
+    for (i = size / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, size, i);
+    }
+
+    for (i = size - 1; i >= 0; i--)
+    {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1, 6, 7, 8, 9, 10, 0};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    heap_sort(arr, len);
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
