f90c9cd9dc1365868784b4c0f2799cc136287440
rtk-rnjn <ritik0ranjan@gmail.com>
2024-01-20 12:01:57 +0530

    Fix mem check in reverse_string.cpp

--- Algorithms/C++/reverse_string.cpp
+++ Algorithms/C++/reverse_string.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int reverse_string(string str, int size)
-{
+void reverse_string(string& str) {
+    int size = str.size();
     char temp;
 
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         temp = str[i];
         str[i] = str[size - i - 1];
         str[size - i - 1] = temp;
     }
-
-    return 0;
 }
 
-int main()
-{
+int main() {
     string str = "Hello World";
 
-    int size = sizeof(str) / sizeof(str[0]) - 1;
-    
-
-    reverse_string(str, size);
+    reverse_string(str);
     cout << str << endl;
 
     return 0;
