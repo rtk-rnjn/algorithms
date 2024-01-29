64e04006dc53b5e7f781050be3743df370bb61e4
rtk-rnjn <ritik0ranjan@gmail.com>
2024-01-29 11:27:30 +0530

    minor

--- CP/LeetCode/CPP/13.cpp
+++ CP/LeetCode/CPP/13.cpp
@@ -7,29 +7,27 @@ using namespace std;
 
 class Solution
 {
-    public:
-        int romanToInt(const string &s)
-        {
-            int result = 0;
-            unordered_map<char, int> symbol{{'I', 1},   {'V', 5},   {'X', 10},
-                                            {'L', 50},  {'C', 100}, {'D', 500},
-                                            {'M', 1000}};
+public:
+    int romanToInt(const string &s)
+    {
+        int result = 0;
+        unordered_map<char, int> symbol{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
-            int size = s.size();
-            for (int i = 0; i < size - 1; i++)
+        int size = s.size();
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (symbol[s[i]] < symbol[s[i + 1]])
             {
-                if (symbol[s[i]] < symbol[s[i + 1]])
-                {
-                    result -= symbol[s[i]];
-                }
-                else
-                {
-                    result += symbol[s[i]];
-                }
+                result -= symbol[s[i]];
             }
-            result += symbol[s[size - 1]];
-            return result;
+            else
+            {
+                result += symbol[s[i]];
+            }
         }
+        result += symbol[s[size - 1]];
+        return result;
+    }
 };
 
 int main()
--- CP/LeetCode/CPP/412.cpp
+++ CP/LeetCode/CPP/412.cpp
@@ -7,31 +7,31 @@ using namespace std;
 
 class Solution
 {
-    public:
-        vector<string> fizzBuzz(int n)
+public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string> result;
+        for (int i = 1; i < n + 1; i++)
         {
-            vector<string> result;
-            for (int i = 1; i < n + 1; i++)
+            if (i % 3 == 0 && i % 5 == 0)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    result.emplace_back("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    result.emplace_back("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    result.emplace_back("Buzz");
-                }
-                else
-                {
-                    result.emplace_back(to_string(i));
-                }
+                result.emplace_back("FizzBuzz");
             }
-            return result;
+            else if (i % 3 == 0)
+            {
+                result.emplace_back("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                result.emplace_back("Buzz");
+            }
+            else
+            {
+                result.push_back(to_string(i));
+            }
         }
+        return result;
+    }
 };
 
 int main()
