3d1eaba9d83bb4f0828652b9e34b305a31297ef3
Ritik Ranjan <ritik0ranjan@gmail.com>
2024-02-03 16:59:06 +0530

    String[] args be final

--- Algorithms/Java/MatrixMultiplication.java
+++ Algorithms/Java/MatrixMultiplication.java
@@ -1,5 +1,5 @@
 public class MatrixMultiplication {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         int[][] matrix1 = { { 1, 2, 3 }, { 4, 5, 6 } };
         int[][] matrix2 = { { 7, 8 }, { 9, 10 }, { 11, 12 } };
