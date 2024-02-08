59bf85b27c5eaa49fc73399fe09c490752aea52a
rtk-rnjn <ritik0ranjan@gmail.com>
2024-02-08 19:21:10 +0530

    Fix CheckStyle linting issue and optimize code in 219.java

--- CP/LeetCode/Java/219.java
+++ CP/LeetCode/Java/219.java
@@ -7,6 +7,7 @@ class Solution {
     public boolean containsNearbyDuplicate(final int[] nums, final int k) {
         Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
+
         for (int i = 0; i < n; i++) {
             if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
                 return true;
