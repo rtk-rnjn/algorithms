343728b84c21d4f788ba16bbcd19a64d1081d01b
Ritik Ranjan <ritik0ranjan@gmail.com>
2024-02-15 09:53:22 +0530

    Update insertion_sort.py

--- Algorithms/Python/insertion_sort.py
+++ Algorithms/Python/insertion_sort.py
@@ -4,15 +4,13 @@ from typing import TypeVar
 
 T = TypeVar("T", bound=int | float)
 
-def insertion_sort(arr):
-    
+
+def insertion_sort(arr: list[T]) -> None:
+    """Insertion sort algorithm to sort the array in ascending order."""
     n = len(arr)
     if n <= 1:
         return arr
-    
-    """
-    Insertion sort algorithm to sort the array in ascending order.
-    """
+
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
@@ -21,7 +19,8 @@ def insertion_sort(arr):
             j -= 1
         arr[j + 1] = key
 
+
 if __name__ == "__main__":
-    arr = [12, 11, 13, 5, 6, 15, 2, 7]     # Input array: Unsorted
+    arr = [12, 11, 13, 5, 6, 15, 2, 7]  # Input array: Unsorted
     insertion_sort(arr)
-    print("Sorted array:", arr)            # Output: Sorted array: [2, 5, 6, 7, 11, 12, 13, 15]
+    print("Sorted array:", arr)  # Output: Sorted array: [2, 5, 6, 7, 11, 12, 13, 15]
