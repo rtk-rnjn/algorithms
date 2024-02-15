e9e51338a10f70428b94698f40d60685c178a1f0
abhirai7 <abhirai5031k@gmail.com>
2024-02-16 00:17:38 +0530

    Stack.cpp

---
+++ DataStructures/CPP/stack.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include<vector>
+
+
+using namespace std;
+
+class Stack{
+    public:
+
+    int size;
+    int top = -1;
+    vector<int> stack;
+
+
+    Stack(int n)
+    {
+        this->size = n;
+    }
+
+    int push(int element)
+    {
+        if (this->top == this->size - 1)
+        {
+            cout<<("Stack Overflow\n");
+            return -1;
+        }
+
+        this->top++;
+        this->stack.push_back(element);
+
+        return 0;
+    }
+
+    int pop()
+    {
+        if (this->top == -1)
+        {
+            cout<<("Stack Underflow\n");
+            return -1;
+        }
+
+        this->top--;
+
+        return 0;
+    }
+
+    int peek()
+    {
+        if (this->top == -1)
+        {
+            cout<<("Stack Underflow\n");
+            return -1;
+        }
+
+        return this->stack[top];
+    }
+
+    void display()
+    {
+        if (this->top == -1)
+        {
+            cout<<("Stack Underflow\n");
+            return;
+        }
+
+        cout<<("The stack is: ");
+        for (int i = 0; i <= this->top; i++)
+        {
+            cout<<(this->stack[i])<<" ";
+        }
+        cout<<("\n");
+    }
+
+};
+
+int main(){
+    Stack *s = new Stack(5);
+    s->push(3);
+    s->push(4);
+    s->push(5);
+    s->pop();
+    cout<<s->peek()<<endl;
+    s->display();
+    delete(s);
+
+
+    return 0;
+}
