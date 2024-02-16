dc8cd5bf387106efefdb0b23fe3e6f5fb2f36ea0
abhirai7 <abhirai5031k@gmail.com>
2024-02-17 02:00:42 +0530

    Fixed the BubbleSort.java file

--- Algorithms/Java/BubbleSort.java
+++ Algorithms/Java/BubbleSort.java
@@ -1,21 +1,19 @@
 class BubbleSort {
     public static void bubbleSort(final int[] arr) {
-        boolean sorted;
+        boolean swapped;
         for (int i = 0; i < arr.length; i++) {
-            sorted = false;
+            swapped = false;
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    sorted = false;
+                    swapped = true;
                     
                 }
-                else{
-                    sorted = true;
-                }
+                
             }
-            if(sorted){
+            if(!swapped){
                 break;
             }
         }
