20f3edc2c7b0a32ce8ec8e26e7b3f4525c1a174c
abhirai7 <abhirai5031k@gmail.com>
2024-02-17 01:52:49 +0530

    Fixed BubbleSort.java

--- Algorithms/Java/BubbleSort.java
+++ Algorithms/Java/BubbleSort.java
@@ -1,18 +1,28 @@
 class BubbleSort {
     public static void bubbleSort(final int[] arr) {
+        boolean sorted;
         for (int i = 0; i < arr.length; i++) {
+            sorted = false;
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    sorted = false;
+                    
                 }
+                else{
+                    sorted = true;
+                }
+            }
+            if(sorted){
+                break;
             }
         }
     }
 
     public static void main(final String[] args) {
-        int[] arr = { 7, 45, 82, 5, 72, 6, 36, 8 };
+        int[] arr = { 7, 45, 82, 5, 72, 6, 36, 8, 100, 99, 98, 97,0};
         bubbleSort(arr);
 
         for (int i : arr) {
