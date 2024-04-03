68589cc45a35db408450a878238e1d0690107d1e
abhirai7 <abhirai5031k@gmail.com>
2024-04-03 08:29:05 +0530

    linear_search(in Python)

--- Algorithms/Python/linear_search.py
+++ Algorithms/Python/linear_search.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 
 def linear_search(arr: list[int], target: int) -> int:
-    for i in enumerate(arr):
-        if(i[1] == target):
-            return i[0]
+    for index,value in enumerate(arr):
+        if(value == target):
+            return index
 
     return -1
 
 
 if __name__ == "__main__":
-    numbers = [1, 2, 3, 4, 5, 6] 
-    print(linear_search(numbers, 3))  
+    numbers = [1, 2, 3,9, 4, 5, 6] 
+    print(linear_search(numbers, 9))  
     print(linear_search(numbers, 7))  
