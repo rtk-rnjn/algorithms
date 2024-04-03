114ec658d7a01d3d599843fdeb5a542ea9438262
abhirai7 <abhirai5031k@gmail.com>
2024-04-03 08:42:00 +0530

    merge_sort(in python)

--- Algorithms/Python/merge_sort.py
+++ Algorithms/Python/merge_sort.py
@@ -1,54 +1,51 @@
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
- 
-    L = [0] * (n1)
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+
+    left = [0] * (n1)
     R = [0] * (n2)
- 
+
     for i in range(0, n1):
-        L[i] = arr[l + i]
- 
+        left[i] = arr[left + i]
+
     for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
- 
-    i = 0     
-    j = 0     
-    k = l     
- 
+        R[j] = arr[mid + 1 + j]
+
+    i = 0
+    j = 0
+    k = left
+
     while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
+        if left[i] <= R[j]:
+            arr[k] = left[i]
             i += 1
         else:
             arr[k] = R[j]
             j += 1
         k += 1
- 
 
     while i < n1:
-        arr[k] = L[i]
+        arr[k] = left[i]
         i += 1
         k += 1
- 
 
     while j < n2:
         arr[k] = R[j]
         j += 1
         k += 1
- 
-
- 
-def merge_sort(arr, l, r):
-    if l < r:
- 
-        m = l+(r-l)//2
- 
-        merge_sort(arr, l, m)
-        merge_sort(arr, m+1, r)
-        merge(arr, l, m, r)
 
 
-arr = [1,54,343,523]
-merge_sort(arr,0,len(arr)-1)
+def merge_sort(arr, left, right):
+    if left < right:
+
+        m = left + (right - left) // 2
+
+        merge_sort(arr, left, m)
+        merge_sort(arr, m + 1, right)
+        merge(arr, left, m, right)
+
+
+arr = [1, 54, 343, 523]
+merge_sort(arr, 0, len(arr) - 1)
 
 print(arr)
