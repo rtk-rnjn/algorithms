ba068c2f52a625785f8f876355b3c2e0e16f5278
abhirai7 <abhirai5031k@gmail.com>
2024-04-19 14:10:47 +0530

    Improved shell_sort(.cpp)

--- Algorithms/C++/shell_sort.cpp
+++ Algorithms/C++/shell_sort.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int shellSort(int arr[], int size)
-{
-	for (int gap = size/2; gap > 0; gap /= 2)
-	{
-		for (int i = gap; i < size; i += 1)
-		{
-			int temp = arr[i];
-
-			int j;		 
-			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
-				arr[j] = arr[j - gap];
-			
-			arr[j] = temp;
-		}
-	}
-	return 0;
+void shellSort(vector<int>& arr) {
+  for (int gap = arr.size() / 2; gap > 0; gap /= 2) {
+    for (int i = gap; i < arr.size(); i += 1) {
+      int temp = arr[i];
+      int j;
+      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+        arr[j] = arr[j - gap];
+      }
+      arr[j] = temp;
+    }
+  }
 }
 
+int main() {
+  vector<int> arr = {12, 34, 54, 2, 3};
 
-int main()
-{
-	int arr[] = {12, 34, 54, 2, 3}, i;
-	int size = sizeof(arr)/sizeof(arr[0]);
+  shellSort(arr);
 
-	shellSort(arr, size);
-    for (int i=0; i<size; i++)
-		cout << arr[i] << " ";
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << " ";
+  }
 
-	return 0;
+  return 0;
 }
