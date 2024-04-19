ecde79ae079ff037fc0a60b764a493b8684a9c73
rtk-rnjn <ritik0ranjan@gmail.com>
2024-04-19 21:28:48 +0530

    Refactor matrix multiplication algorithm in C to use dynamic memory allocation

--- Algorithms/C/matrix_multiplication_recursive.c
+++ Algorithms/C/matrix_multiplication_recursive.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void add(int n, int A[][n], int B[][n], int C[][n])
+void free_matrix(int n, int **matrix);
+
+void add(int n, int **A, int **B, int **C) // NOLINT
 {
     for (int i = 0; i < n; i++)
     {
@@ -12,7 +14,7 @@ void add(int n, int A[][n], int B[][n], int C[][n])
     }
 }
 
-void subtract(int n, int A[][n], int B[][n], int C[][n])
+void subtract(int n, int **A, int **B, int **C) // NOLINT
 {
     for (int i = 0; i < n; i++)
     {
@@ -23,7 +25,17 @@ void subtract(int n, int A[][n], int B[][n], int C[][n])
     }
 }
 
-void multiply(int n, int A[][n], int B[][n], int C[][n])
+int **allocate(const int n)
+{
+    int **matrix = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
+    {
+        matrix[i] = (int *)malloc(n * sizeof(int));
+    }
+    return matrix;
+}
+
+void multiply(int n, int **A, int **B, int **C) // NOLINT
 {
     if (n == 1)
     {
@@ -33,30 +45,30 @@ void multiply(int n, int A[][n], int B[][n], int C[][n])
 
     // Divide matrices into submatrices
     int new_size = n / 2;
-    int A11[new_size][new_size];
-    int A12[new_size][new_size];
-    int A21[new_size][new_size];
-    int A22[new_size][new_size];
-    int B11[new_size][new_size];
-    int B12[new_size][new_size];
-    int B21[new_size][new_size];
-    int B22[new_size][new_size];
+    int **A11 = allocate(new_size);
+    int **A12 = allocate(new_size);
+    int **A21 = allocate(new_size);
+    int **A22 = allocate(new_size);
+    int **B11 = allocate(new_size);
+    int **B12 = allocate(new_size);
+    int **B21 = allocate(new_size);
+    int **B22 = allocate(new_size);
 
-    int C11[new_size][new_size];
-    int C12[new_size][new_size];
-    int C21[new_size][new_size];
-    int C22[new_size][new_size];
+    int **C11 = allocate(new_size);
+    int **C12 = allocate(new_size);
+    int **C21 = allocate(new_size);
+    int **C22 = allocate(new_size);
 
-    int P1[new_size][new_size];
-    int P2[new_size][new_size];
-    int P3[new_size][new_size];
-    int P4[new_size][new_size];
-    int P5[new_size][new_size];
-    int P6[new_size][new_size];
-    int P7[new_size][new_size];
+    int **P1 = allocate(new_size);
+    int **P2 = allocate(new_size);
+    int **P3 = allocate(new_size);
+    int **P4 = allocate(new_size);
+    int **P5 = allocate(new_size);
+    int **P6 = allocate(new_size);
+    int **P7 = allocate(new_size);
 
-    int temp1[new_size][new_size];
-    int temp2[new_size][new_size];
+    int **temp1 = allocate(new_size);
+    int **temp2 = allocate(new_size);
 
     // Divide matrix A into 4 submatrices
     for (int i = 0; i < new_size; i++)
@@ -124,6 +136,40 @@ void multiply(int n, int A[][n], int B[][n], int C[][n])
             C[i + new_size][j + new_size] = C22[i][j];
         }
     }
+
+    free_matrix(new_size, A11);
+    free_matrix(new_size, A12);
+    free_matrix(new_size, A21);
+    free_matrix(new_size, A22);
+    free_matrix(new_size, B11);
+    free_matrix(new_size, B12);
+    free_matrix(new_size, B21);
+    free_matrix(new_size, B22);
+
+    free_matrix(new_size, C11);
+    free_matrix(new_size, C12);
+    free_matrix(new_size, C21);
+    free_matrix(new_size, C22);
+
+    free_matrix(new_size, P1);
+    free_matrix(new_size, P2);
+    free_matrix(new_size, P3);
+    free_matrix(new_size, P4);
+    free_matrix(new_size, P5);
+    free_matrix(new_size, P6);
+    free_matrix(new_size, P7);
+
+    free_matrix(new_size, temp1);
+    free_matrix(new_size, temp2);
+}
+
+void free_matrix(int n, int **matrix)
+{
+    for (int i = 0; i < n; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
 }
 
 int main()
@@ -131,19 +177,35 @@ int main()
     int n = 4;
     int A[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
     int B[4][4] = {{17, 18, 19, 20}, {21, 22, 23, 24}, {25, 26, 27, 28}, {29, 30, 31, 32}};
-    int C[4][4];
 
-    multiply(n, A, B, C);
+    int **A_matrix = allocate(n);
+    int **B_matrix = allocate(n);
+    int **C_matrix = allocate(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            A_matrix[i][j] = A[i][j];
+            B_matrix[i][j] = B[i][j];
+        }
+    }
+
+    multiply(n, A_matrix, B_matrix, C_matrix);
 
     printf("Resultant matrix C:\n");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf("%d ", C[i][j]);
+            printf("%d ", C_matrix[i][j]);
         }
         printf("\n");
     }
+
+    free_matrix(n, A_matrix);
+    free_matrix(n, B_matrix);
+    free_matrix(n, C_matrix);
 
     return 0;
 }
