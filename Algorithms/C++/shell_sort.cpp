419c0c764bbfa3dc83d73b2fce7e99155bbd1c13
Ritik Ranjan <37435729+rtk-rnjn@users.noreply.github.com>
2024-04-19 15:26:33 +0530

    Update shell_sort.cpp

--- Algorithms/C++/shell_sort.cpp
+++ Algorithms/C++/shell_sort.cpp
@@ -3,27 +3,34 @@
 
 using namespace std;
 
-void shellSort(vector<int>& arr) {
-  for (int gap = arr.size() / 2; gap > 0; gap /= 2) {
-    for (int i = gap; i < arr.size(); i += 1) {
-      int temp = arr[i];
-      int j;
-      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
-        arr[j] = arr[j - gap];
-      }
-      arr[j] = temp;
+void shellSort(vector<int> &arr)
+{
+    int size = (int)arr.size();
+    for (int gap = size / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < arr.size(); i += 1)
+        {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+            {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
     }
-  }
 }
 
-int main() {
-  vector<int> arr = {12, 34, 54, 2, 3};
+int main()
+{
+    vector<int> arr = {12, 34, 54, 2, 3};
 
-  shellSort(arr);
+    shellSort(arr);
 
-  for (int i = 0; i < arr.size(); i++) {
-    cout << arr[i] << " ";
-  }
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
 
-  return 0;
+    return 0;
 }
