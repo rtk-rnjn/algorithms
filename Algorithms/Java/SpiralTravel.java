8cb581ca0e80019dc6efd7f563fea26c69292bac
rtk-rnjn <ritik0ranjan@gmail.com>
2024-04-21 00:30:19 +0530

    Add SpiralTravel algorithm implementation in Java

---
+++ Algorithms/Java/SpiralTravel.java
@@ -0,0 +1,45 @@
+class SpiralTravel {
+    public static void main(final String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
+
+        spiralTravel(matrix);
+    }
+
+    public static void spiralTravel(final int[][] matrix) {
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+}
