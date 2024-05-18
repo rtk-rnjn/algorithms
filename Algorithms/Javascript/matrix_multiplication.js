26d65435aec1f2e66e85e916be1cc2211ccfc8e9
codefactor-io <support@codefactor.io>
2024-05-18 06:26:09 +0000

    [CodeFactor] Apply fixes to commit d147445

--- Algorithms/Javascript/matrix_multiplication.js
+++ Algorithms/Javascript/matrix_multiplication.js
@@ -1,21 +1,21 @@
-const MM = (arr1,arr2) =>{
-    let n = arr1.length;
-    let resultarr = new Array(n);
-    for(let i = 0;i<n;i++){
+const MM = (arr1,arr2) => {
+    const n = arr1.length;
+    const resultarr = new Array(n);
+    for (let i = 0; i < n; i++) {
         resultarr[i] = new Array(n);
     }
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             resultarr[i][j] = 0;
-            for(let k = 0;k<n;k++){
-                resultarr[i][j]+=arr1[i][k]*arr2[k][j];
+            for (let k = 0; k < n; k++) {
+                resultarr[i][j] += arr1[i][k] * arr2[k][j];
             }
         }
     }
     return resultarr;
-}
+};
 
-let A = [[1,2],[3,4]]
-let B = [[1,2],[3,4]]
-let C = MM(A,B);
+const A = [[1,2],[3,4]];
+const B = [[1,2],[3,4]];
+const C = MM(A,B);
 console.log(C);
