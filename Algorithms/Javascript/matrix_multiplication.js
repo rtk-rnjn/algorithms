ede06a54d8c5c8a1a2edafa1c4892936472db34f
abhirai7 <abhirai5031k@gmail.com>
2024-07-27 23:13:58 +0530

    chore: Optimize matrix multiplication algorithm in JavaScript

--- Algorithms/Javascript/matrix_multiplication.js
+++ Algorithms/Javascript/matrix_multiplication.js
@@ -1,21 +1,27 @@
-const MM = (arr1,arr2) =>{
-    let n = arr1.length;
-    let resultarr = new Array(n);
-    for(let i = 0;i<n;i++){
-        resultarr[i] = new Array(n);
+const MM = (arr1, arr2) => {
+  let n = arr1.length;
+  let resultarr = new Array(n);
+  for (let i = 0; i < n; i++) {
+    resultarr[i] = new Array(n);
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      resultarr[i][j] = 0;
+      for (let k = 0; k < n; k++) {
+        resultarr[i][j] += arr1[i][k] * arr2[k][j];
+      }
     }
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            resultarr[i][j] = 0;
-            for(let k = 0;k<n;k++){
-                resultarr[i][j]+=arr1[i][k]*arr2[k][j];
-            }
-        }
-    }
-    return resultarr;
-}
+  }
+  return resultarr;
+};
 
-let A = [[1,2],[3,4]]
-let B = [[1,2],[3,4]]
-let C = MM(A,B);
+let A = [
+  [1, 2],
+  [3, 4],
+];
+let B = [
+  [1, 2],
+  [3, 4],
+];
+let C = MM(A, B);
 console.log(C);
