032186cbf928664429b5afa9e3024b2211af83d4
rtk-rnjn <ritik0ranjan+github@gmail.com>
2024-07-29 11:39:29 +0530

    chore: Refactor binarySearch.js to use arrow function syntax

--- Algorithms/Javascript/searching/binarySearch.js
+++ Algorithms/Javascript/searching/binarySearch.js
@@ -1,13 +1,12 @@
 
-function binarySearch(arr, target) {
+const binarySearch = (arr, target) => {
     let left = 0;
     let right = arr.length - 1;
 
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
         if (arr[mid] === target) {
-            console.log(`Found ${target} at index ${mid}`);
-            return;
+            return mid;
         }
         if (arr[mid] < target) {
             left = mid + 1;
@@ -16,8 +15,15 @@ function binarySearch(arr, target) {
         }
     }
 
-    console.log(`Not found ${target}`);
-}
+    return -1;
+};
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-binarySearch(arr, 10);
+
+index = binarySearch(arr, 5);
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+}
+else {
+    console.log('Element not found');
+}
