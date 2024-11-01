816b76c31f9163697e09b13f487bfecee7afee1b
rtk-rnjn <ritik0ranjan+github@gmail.com>
2024-11-01 13:59:14 +0530

    Add ignore patterns to ESLint configuration and refactor GitHub data fetching in utils.jinja

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
         // use 4 spaces indent
         indent: ['error', 4],
         quotes: [2, 'single', { avoidEscape: true }]
-    }
+    },
+    ignorePatterns: ['node_modules/', 'particles.js', 'particles.json']
 };
