5588b37a8bcceaf3d0e1d857f62307d538e43d3f
rtk-rnjn <ritik0ranjan+github@gmail.com>
2025-05-25 00:58:56 +0530

    Add solution for "Remove Element" problem in Java

---
+++ CP/LeetCode/Java/27.java
@@ -0,0 +1,21 @@
+// 27. Remove Element
+
+class Solution {
+    public int removeElement(final int[] nums, int val) {
+        int insertPos = 0;
+        for (int current : nums) {
+            if (current != valToRemove) {
+                nums[insertPos++] = current;
+            }
+        }
+        return insertPos;
+    }
+
+    public static void main(final String[] args) {
+        Solution sol = new Solution();
+        int[] nums = { 3, 2, 2, 3 };
+        int valToRemove = 3;
+
+        System.out.println(sol.removeElement(nums, valToRemove));
+    }
+}
